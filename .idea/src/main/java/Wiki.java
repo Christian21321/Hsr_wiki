b6643ca9a3ba1e6ffe98ef267d657edd7c799656
Hector <143755568+mnomann@users.noreply.github.com>
2025-04-22 01:17:54 -0400

    creacion de tests

---
+++ .idea/src/main/java/Wiki.java
@@ -0,0 +1,15 @@
+public class Wiki{
+    public void subirNivelPersonaje(Personaje personaje, int nivelesASubir) {
+        if (nivelesASubir > 0) {
+            personaje.setNivel(personaje.getNivel() + nivelesASubir);
+        }
+    }
+
+    public void agregarConoAVia(Via via, Cono cono) {
+        if (via.getConos() == null) {
+            via.setConos(new ArrayList<>());
+        }
+        via.getConos().add(cono);
+    }
+    
+}
